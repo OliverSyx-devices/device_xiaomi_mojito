16eae75059a0eccc7c8e9720bd4dbb4b67e14e90
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    mojito: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -298,6 +298,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor \
     android.hardware.keymaster@4.1.vendor
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
