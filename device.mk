4d905618451129297d2ea04e0cbabad66faf0e1d
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2025-09-01 21:34:08 +0700

    mojito: nuke during apps runtime
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -416,6 +416,10 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor
 
+# Remove packages
+PRODUCT_PACKAGES += \
+    rmv
+
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge
