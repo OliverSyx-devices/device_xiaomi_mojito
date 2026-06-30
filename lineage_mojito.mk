a8d1b7a3c2e31b4983d83f414cff3b4d78d9a4aa
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2026-06-30 23:41:06 +0700

    mojito: Rising-LTS

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -18,7 +18,10 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
-
+RISING_CHIPSET := Snapdragon 678
+RISING_MAINTAINER := OliverSyx
+RISING_PACKAGE_TYPE := VANILLA AOSP
+WITH_GMS := false
 TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_NAME := lineage_mojito
